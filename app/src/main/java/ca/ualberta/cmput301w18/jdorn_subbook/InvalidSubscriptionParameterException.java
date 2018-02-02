9cd693af9ed1168705621f227e7d1019a81f3eed
abxjdorn <abxjdorn@users.noreply.github.com>
2018-02-02 14:56:21 -0700

    remaining documentation and style

--- app/src/main/java/ca/ualberta/cmput301w18/jdorn_subbook/InvalidSubscriptionParameterException.java
+++ app/src/main/java/ca/ualberta/cmput301w18/jdorn_subbook/InvalidSubscriptionParameterException.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cmput301w18.jdorn_subbook;
 
-
+/**
+ * Exception thrown when failing to instantiate or update a Subscription object.
+ */
 public class InvalidSubscriptionParameterException extends Throwable {
 }
--- app/src/main/java/ca/ualberta/cmput301w18/jdorn_subbook/SubscriptionListLoader.java
+++ app/src/main/java/ca/ualberta/cmput301w18/jdorn_subbook/SubscriptionListLoader.java
@@ -16,18 +16,38 @@ import java.io.IOException;
  * Utility to load a SubscriptionList from a file
  * and save it back to a file.
  */
-public class SubscriptionListLoader {
-    private String filename;
-    private Context context;
-    private Gson gson;
+class SubscriptionListLoader {
+    /** Target filename for saving and loading. */
+    private final String filename;
     
-    public SubscriptionListLoader(Context context, String filename) {
+    /** Context used to request file streams. */
+    private final Context context;
+    
+    /** GSON de/serializer. */
+    private final Gson gson;
+    
+    /**
+     * Creates a SubscriptionListLoader for the given filename.
+     *
+     * @param context context used to request file streams
+     * @param filename target file name
+     */
+    SubscriptionListLoader(Context context, String filename) {
         this.context = context;
         this.filename = filename;
         this.gson = new Gson();
     }
     
-    public SubscriptionList load() throws IOException {
+    /**
+     * Restores the list of subscriptions stored in the target file.
+     * If the file does not exist, returns an empty list. (This allows
+     * this method to be transparent to whether any data has been saved
+     * yet.)
+     *
+     * @return list restored from the target file (or empty list if file does not exist)
+     * @throws IOException on failure to open or read the file
+     */
+    SubscriptionList load() throws IOException {
         try {
             FileInputStream stream = context.openFileInput(this.filename);
             FileReader fileReader = new FileReader(stream.getFD());
@@ -39,7 +59,13 @@ public class SubscriptionListLoader {
         }
     }
     
-    public void save(SubscriptionList list) throws IOException {
+    /**
+     * Saves the given list of subscriptions to the target file.
+     *
+     * @param list list of subscriptions to save
+     * @throws IOException on failure to open or write the file
+     */
+    void save(SubscriptionList list) throws IOException {
         FileOutputStream stream = context.openFileOutput(this.filename, Context.MODE_PRIVATE);
         FileWriter fileWriter = new FileWriter(stream.getFD());
         fileWriter.write(this.gson.toJson(list));
